25ca63ad18a24d5b1383d0308f838e032ae0937a
Thomas DOUCHE <Thomas.douche@aol.com>
2024-10-28 21:16:41 +0100

    conact link

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -9,7 +9,7 @@ export default function Footer() {
             <Separator size='4' mt='4' />
             <Flex justify='center' p='4' gap='4'>
                 <Link href='/' size='1'>home</Link>
-                <Link href='https://www.tomx.sh' size='1'>contact me</Link>
+                <Link href='https://www.tomx.sh/contact' size='1'>contact me</Link>
                 <Link href='https://www.tomx.sh' size='1'>tomx.sh</Link>
             </Flex>
         </footer>
