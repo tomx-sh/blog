cf096f17c06c08a54caa616315536aaf09ab08d5
Thomas DOUCHE <Thomas.douche@aol.com>
2024-12-30 20:32:53 +0100

    add pending animation

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -2,12 +2,15 @@
 import { Container, Section, TextField, Button, Flex } from "@radix-ui/themes"
 import { revalidate } from "../api/revalidate/action"
 import { useRouter } from "next/navigation"
+import { useState } from "react"
 
 export default function Page() {
     const router = useRouter()
+    const [isPending, setIsPending] = useState(false)
 
     const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault()
+        setIsPending(true)
         const formData = new FormData(event.currentTarget)
         const path = formData.get('path') as string
         const password = formData.get('password') as string
@@ -17,6 +20,7 @@ export default function Page() {
         } else {
             router.push(path)
         }
+        setIsPending(false)
     }
 
 
@@ -27,7 +31,7 @@ export default function Page() {
                     <Flex gap='3' direction='column' maxWidth='300px' mx='auto'>
                         <TextField.Root placeholder='path' name='path' />
                         <TextField.Root placeholder='password' name='password' type="password" />
-                        <Button type="submit">Revalidate</Button>
+                        <Button type="submit" loading={isPending}>Revalidate</Button>
                     </Flex>
                 </form>
             </Section>
